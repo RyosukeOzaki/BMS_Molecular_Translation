caf7a0e0056d6fa10a27156bed2446d14da03332
RyosukeOzaki <ryoryoryor102510@gmail.com>
2021-04-09 21:39:59 +0900

    split .py

---
+++ script/utils.py
@@ -0,0 +1,72 @@
+import os
+import numpy as np
+import random
+
+import torch
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from albumentations import ImageOnlyTransform
+import Levenshtein
+
+
+# ====================================================
+# Utils
+# ====================================================
+def get_test_file_path(image_id):
+    return "../input/bms-molecular-translation/test/{}/{}/{}/{}.png".format(
+        image_id[0], image_id[1], image_id[2], image_id 
+    )
+
+def get_score(y_true, y_pred):
+    scores = []
+    for true, pred in zip(y_true, y_pred):
+        score = Levenshtein.distance(true, pred)
+        scores.append(score)
+    avg_score = np.mean(scores)
+    return avg_score
+
+
+def init_logger(log_file='inference.log'):
+    from logging import getLogger, INFO, FileHandler,  Formatter,  StreamHandler
+    logger = getLogger(__name__)
+    logger.setLevel(INFO)
+    handler1 = StreamHandler()
+    handler1.setFormatter(Formatter("%(message)s"))
+    handler2 = FileHandler(filename=log_file)
+    handler2.setFormatter(Formatter("%(message)s"))
+    logger.addHandler(handler1)
+    logger.addHandler(handler2)
+    return logger
+
+def seed_torch(seed=42):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = False # True
+
+# ====================================================
+# Transforms
+# ====================================================
+def get_transforms(size, data):
+    
+    if data == 'train':
+        return A.Compose([
+            A.Resize(size, size),
+            A.Normalize(
+                mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225],
+            ),
+            ToTensorV2(),
+        ])
+    
+    elif data == 'valid':
+        return A.Compose([
+            A.Resize(size, size),
+            A.Normalize(
+                mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225],
+            ),
+            ToTensorV2(),
+        ])
